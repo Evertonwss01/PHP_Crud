203f222a69b8eea19303445d67aad4f52fa778c8
Evertonwss01 <evertonwss2001@gmail.com>
2023-01-04 10:50:08 -0300

    BancoDados
    Banco de dados Utilizado no Projeto.

---
+++ PaginaWeb/BancoDadosFuncionario.sql
@@ -0,0 +1,17 @@
+create table funcionario(
+id_funcionario int auto_increment primary key,
+nome_funcionario varchar(45),
+rg_funcionario varchar(20),
+email varchar(30),
+senha varchar(30),
+idade int,
+sexo varchar(12)
+);
+
+use SistemaPhp;
+select * from funcionario;
+
+alter table funcionario add senha varchar(30) after email;
+
+/*Query utilizada na validação do Usuario se existia ou não no Banco*/
+/*SELECT * FROM funcionario WHERE email = 'junior.melo@gmail.com' AND senha = 'geraldinho';*/
