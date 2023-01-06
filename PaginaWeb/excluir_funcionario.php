b4743869fee51a9ded8126e56592cfa1283c0a8e
Evertonwss01 <evertonwss2001@gmail.com>
2023-01-06 10:05:17 -0300

    AtualizaçaoExcluir
    Passando a função de Deletar um registro no Banco de Dados, para o segundo iconi criado na ultima modificação.

--- PaginaWeb/excluir_funcionario.php
+++ PaginaWeb/excluir_funcionario.php
@@ -1,14 +1,17 @@
 <?php
 
-    require 'Conexao.php';
+require 'Conexao.php';
 
-    $id = $_GET["id"];
-    $sql = "delete from funcionario where id_funcionario = $id";
+$id = $_GET["id"];
+$sql = "delete from funcionario where id_funcionario = $id";
 
-    $resultado = mysqli_query($conexao, $sql);
+$resultado = mysqli_query($conexao, $sql);
 
-    if($resultado == true){
-        echo "Registro Apagado !";
-    }else{echo"Erro ao Apagar o Registro !";}
+if($resultado == true){
+    //echo "Registro Apagado !";
+    header('Location: listar_funcionario.php');
+}else{
+    echo"Erro ao Apagar o Registro !";
+    echo "<a href='listar_funcionario.php'><br>Voltar a Lista</a>";
+}
 ?>
-<a href="listar_funcionario.php"><br>Voltar a Lista</a>
