fe9c171b26019c5ac77b2276b26ab804548652ef
Evertonwss01 <evertonwss2001@gmail.com>
2023-06-05 13:38:18 -0300

    Organizando as pastas

---
+++ Crud-mvc-php/models/Cliente.php
@@ -0,0 +1,7 @@
+<!-- Consultas | Regras de Negocio -->
+
+<?php 
+    /* Como essa pagina ficará encarregada de fazer as consultas no banco de dados, ela precisará,
+    fazer um requerimento para a classe de Conexão. Lá na pasta de "Configuration".*/
+    require_once ('./')
+?>
